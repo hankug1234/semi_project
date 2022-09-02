fcc64b7ce86b30957fe1ca532ef5f80b4003cd2c
hankug1234 <hankug12@gmail.com>
2022-09-02 11:25:14 +0900

    moving avg append

--- Datareader.py
+++ Datareader.py
@@ -3,17 +3,48 @@ import pandas as pd
 
 class Datareader:
     def __init__(self):
+        #data_stor
         self.dr = fdr.DataReader
         self.sl = fdr.StockListing
         self.fc_list = None
         self.cur_s_data = None
+        self.fc_code = None
+        self.current_graph_data =  pd.DataFrame({'Date':[0],'Close':[0]})
+        self.default_graph = pd.DataFrame({'Date':[0],'Close':[0]})
+        self.current_show_stock_list = None
+        self.current_m_avg_graph_data = None
+        self.multi_state = False
+
+    def on_multi_state(self):
+        self.multi_state = True
+    def off_multi_state(self):
+        self.multi_state  = False
+    def read_fc(self,f_code):
+        if self.fc_list is None:
+            self.fc_code = f_code
+            self.fc_list = self.sl(f_code)
+            self.fc_list.set_index('Name',inplace=True)
 
     def get_graph_data(self,f_code,*s_name):
         self.read_stock_datas(f_code,*s_name)
-        return self.to_graph_type(*s_name)
+        if(self.multi_state):
+            self.current_show_stock_list.extend([*s_name])
+            self.current_graph_data = self.to_graph_type(*self.current_show_stock_list).loc[:,['Close','name','Date']]
+        else:
+            self.current_graph_data = self.to_graph_type(*s_name).loc[:,['Close','name','Date']]
+            self.current_show_stock_list = [*s_name]
+        return self.current_graph_data
+
+    def get_graph_mavg_data(self,f_code,s_name,*n_avgs):
+        self.read_stock_datas(f_code,s_name)
+        result = [self.n_m_avg(n,s_name, 'Close') for n in n_avgs]
+        result.append(self.cur_s_data[s_name].loc[:,['Close','name','Date']])
+        self.current_m_avg_graph_data = pd.concat(result,axis=0)
+        return self.current_m_avg_graph_data
 
     def read_stock_datas(self,f_code,*s_name):
         if self.fc_list is None:
+            self.fc_code = f_code
             self.fc_list = self.sl(f_code)
             self.fc_list.set_index('Name',inplace=True)
 
@@ -70,6 +101,10 @@ class Datareader:
             for i, c in enumerate(df.columns):
                 if c == column:
                     c_n = i
-                    return [df.iloc[i:i + (n - 1), c_n].sum() / len(df[column]) for i in range(len(df) - n)]
+                    mavg = pd.DataFrame(data={'Close':[df.iloc[i:i + n, c_n].sum() / n for i in range(len(df) - n+1)]})
+                    mavg['Date'] = df.index[:len(df)-n+1]
+                    mavg['name'] = f'mavg{n}'
+                    mavg.index = mavg['Date']
+                    return mavg
         return None
 
--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc, callback
+from dash import Dash, html, dcc, callback, ctx
 from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
 import Datareader as dr
 
 data = dr.Datareader()
+data.read_fc('KRX')
+default_graph = px.line(data.default_graph, x='Date', y="Close")
 
 def generate_table(dataframe, max_rows=10):
     return html.Table([
@@ -23,11 +25,6 @@ def generate_table(dataframe, max_rows=10):
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = data.get_graph_data('KRX','힘스','3S')
-fig = px.line(df, x='Date', y="Close",color='name')
-
 app.layout = html.Div(children=[
 
     html.H1(children='STOCK ANALYSIS',
@@ -40,20 +37,60 @@ app.layout = html.Div(children=[
                        ,style={'width': '80%'}), html.Button(id='submit',n_clicks=0,value='SEARCH',style={'width':'50px','height':'20px'})]
              ,style={'textAlign':'center'}
              ),
-
-    html.Div(dcc.Graph( id='example-graph', figure=fig)),
+    html.Div([dcc.RadioItems([
+        {
+            "label": 'ONLY STOCK',
+            "value": "stock",
+        },
+        {
+            "label": 'MOVING AVG',
+            "value": "m_avg",
+        },
+        {
+            "label": 'MUTY STOCK GRAGP',
+            "value": "multi",
+        }
+    ],value="stock",id='radio', inline=True
+     ,style={'margin':'10px','margin-left':'10%','width':'40%','color': 'white', 'font-size': 10,}
+    )]
+    ),
+    html.Div(dcc.Graph( id='example-graph')),
     html.Div()
 ],style={'background-color':'black'})
 
 @app.callback(
     Output(component_id='example-graph',component_property='figure'),
     Input(component_id='submit',component_property='n_clicks'),
-    State(component_id="stock_name",component_property='value')
+    State(component_id="stock_name",component_property='value'),
+    Input(component_id='radio',component_property='value')
               )
+def switch(n_click , name, value):
+    trigger_id = ctx.triggered_id
+    if trigger_id == 'submit':
+        return search_stock(n_click,name)
+    elif trigger_id == 'radio':
+        return change_mode(value)
+    else:
+        return default_graph
 def search_stock(n_click ,name):
     if(n_click==0):
-        return fig
-    return px.line(data.get_graph_data('KRX',name), x='Date', y="Close",color='name')
+        return default_graph
+    return px.line(data.get_graph_data('KRF',name), x='Date', y="Close",color='name')
+
+def change_mode(value):
+    if value == 'stock':
+        data.off_multi_state()
+        return px.line(data.current_graph_data, x='Date', y="Close",color='name')
+    elif value == 'm_avg':
+        data.off_multi_state()
+        if data.current_show_stock_list is None:
+            return px.line(data.current_graph_data, x='Date', y="Close",color='name')
+        else:
+            return px.line(data.get_graph_mavg_data('KRF',data.current_show_stock_list[-1],5,20,60), x='Date', y="Close",color='name')
+    else:
+        data.on_multi_state()
+        return px.line(data.current_graph_data, x='Date', y="Close",color='name')
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
